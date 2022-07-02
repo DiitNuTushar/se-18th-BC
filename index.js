88dd8033c3938d96caba87512a046537100b9f66
maksudul99robots <maksudul@99robots.com>
2022-07-02 22:20:36 +0600

    changes made and bug fixed

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 // const {login} = require('./modules/auth');
-
+var cors = require('cors')
+// app.use(cors())
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors())
+// /auth/
 app.use("/auth", require("./routes/user.route"));
+// app.use("/project", require("./routes/project.route"));
+
 
 app.use((req, res) => {
     res.status(404).json({
--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,8 @@ const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 // const axios = require('axios');
 
-// Login a user
+// Login a user route : /auth/login
+
 router.post(
     "/login", [
         //validation
@@ -16,12 +17,24 @@ router.post(
         }),
     ],
     (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
         const error = validationResult(req);
         if (!error.isEmpty()) {
             return res.status(400).json({
                 error: error.array(),
             });
         }
+
+        // let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im10dXNoYXI3OEBnbWFpbC5jb20iLCJ1c2VyX25hbWUiOiJ0dXNoYXIiLCJpYXQiOjE2NTY3NDQ3MTUsImV4cCI6MTY1NzEwNDcxNX0.VbdjTYJn_lqFEVcbWE7Xq6QPiepXl6DtvlbUtr_ew-g";
+        // jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTY3NDM5MzgsImV4cCI6MTY1NzEwMzkzOH0.2Hgd8jJLIsq_Xbh7s-p0Ivb5lYaQZtPd7-3fGfD7cB8", 
+        // "DIIT18thBatchB&C", function(err, decoded) {
+        //     console.log(decoded) // bar
+        //   });
+
+        //   jwt.verify(token, 'DIIT18thBatchB&C', function(err, decoded) {
+        //     console.log(decoded) // bar
+        //   });
+        // console.log(decoded)
         let query = `select * from users where email = '${req.body.email}' and password = '${req.body.password}'`;
         // making the db query
         con.query(query,
@@ -31,11 +44,12 @@ router.post(
                 }
                 // console.log(typeof(result));
                 if (result.length > 0) {
+                    // payload is the data passed in, for creating a token
                     const payload = {
-                        email: result.email,
-                        user_name: result.user_name,
+                        email: result[0].email,
+                        user_name: result[0].user_name,
                     };
-
+                    //Creating a json web token.
                     jwt.sign(
                         payload,
                         "DIIT18thBatchB&C", {
@@ -69,6 +83,7 @@ router.post(
         }),
     ],
     (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
         const error = validationResult(req);
         if (!error.isEmpty()) {
             return res.status(400).json({
